896f1281dba8b9c0e05be24d8e635de98b55d398
hanxu <apple@appledeiMac.local>
2018-01-09 17:23:46 +0800

    no message

--- 002/pages/index/index.js
+++ 002/pages/index/index.js
@@ -8,8 +8,11 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     list:[],
     products:[],
+    answer:[],
     page:1,
-    per_page:10
+    per_page:10,
+    value1: '',
+    isMore:true
   },
   //事件处理函数
   bindViewTap: function() {
@@ -27,7 +30,10 @@ Page({
       if (data.data.error_code === 0){
         that.data.products = that.data.products.concat(data.data.products)
         console.log(that.data.products)
-        that.setData({ list: that.data.products })
+        that.setData({ 
+          list: that.data.products,
+          isMore: true
+        })
       }
     });
   },
@@ -42,5 +48,36 @@ Page({
   lookMore:function(){
     this.data.page ++
     this.getData()
+  },
+  closeMore:function(){
+    this.data.page=1
+    this.getData()
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  enter: function () {
+    const that = this
+    if (this.data.inputValue === '') {
+      return
+    }
+    const url = 'https://wx.365yf.cc/wx_api/symbols/search?keyword_code=' + this.data.inputValue
+    util.getData(url).then(function (data) {
+      console.log(data.data)
+      if (data.data.error_code === 0) {
+        that.data.answer = data.data.products
+      }
+      else {
+        that.data.answer = data.data.error_reason
+      }
+      that.setData({
+        list: [].concat(that.data.answer),
+        value1: '',
+        inputValue: '',
+        isMore:false
+      })
+    });
   }
 })
